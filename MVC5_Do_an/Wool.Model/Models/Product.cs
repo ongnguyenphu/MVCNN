9d9ab08a2f5b94e1559016ac25dab92363df27ff
Pong <nguyenphu1012@hotmail.com>
2017-03-19 16:00:57 +0700

    Commit the first two classes

---
+++ MVC5_Do_an/Wool.Model/Models/Product.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wool.Model
+{
+    public class Product
+    {
+        public long ID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public bool Status { get; set; }
+        public long Condition { get; set; }
+        public long Brand { get; set; }
+        public double Price { get; set; }
+
+        public long CategoryID { get; set; }
+        public Category Category { get; set; }
+
+    }
+}
