8d694bacc416bb886e8eb77eb0df964d1dffe696
Pong <nguyenphu1012@hotmail.com>
2017-03-26 18:57:10 +0700

    add untrack file

---
+++ MVC5_Do_an/Wool.Web/ViewModels/ProductViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Wool.Web.ViewModels
+{
+    public class ProductViewModel
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public string Image { get; set; }
+
+        public long CategoryId { get; set; }
+    }
+}
